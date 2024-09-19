ed38757f41f387bc3cef0c8273a5c679da6a2ea2
Ivan Fuziyama <IvanSeiji21@gmail.com>
2024-09-19 16:02:08 -0300

    operadores parte'

--- cursoemvideo/ex008/script.js
+++ cursoemvideo/ex008/script.js
@@ -1,2 +1,10 @@
-var vel = Number(document.getElementById('velocidade').value)
-console.log('vel')
+
+function Verificar(){
+    var vel = Number(document.getElementById('velocidade').value)
+    var res = document.getElementById('resultado')
+    res.innerHTML = `<p>Sua velocidade é de <strong>${vel}</strong>Km/h</p>`
+    if(vel>=60){
+        res.innerHTML += `<p>Você foi MULTADO por excesso de velocidade</p>`
+    }
+    res.innerHTML += `<p>Dirija com segurança!</p>`
+}
