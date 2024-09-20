b113e1c7b7c7a53ce2681f61fdbf4b9985ce7107
Ivan Fuziyama <IvanSeiji21@gmail.com>
2024-09-20 12:15:32 -0300

    tabuada

---
+++ cursoemvideo/ex-tabuada/script.js
@@ -0,0 +1,13 @@
+function Calcular(){
+    var n = Number(document.getElementById('numero').value)
+    var tab = document.getElementById('tab')
+    var f = document.getElementById('fim')
+    tab.innerHTML = ''
+
+    for(var x = 0;x <= 10;x++){
+        var item = document.createElement('option')
+        item.text = `${n} x ${x} = ${n*x}`
+        item.value = `tab${x}`
+        tab.appendChild(item)
+    }
+}
